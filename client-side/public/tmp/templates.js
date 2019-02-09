965eb4ae27f17eb4df9af52092a7903e978cdfdd
SpasZahariev <spaszahariev54@gmail.com>
2019-02-09 18:45:27 +0000

    fixed contact text on main page

--- client-side/public/tmp/templates.js
+++ client-side/public/tmp/templates.js
@@ -7,17 +7,38 @@ templates["choose_receiver.handlebars"] = "<h1>Choose Receiver</h1>\n" +
    "";
 
 templates["enter_questions.handlebars"] = "<div class=\"container\">\n" +
-   "  <div class=\"row\">\n" +
-   "    <div class=\"col-4\">\n" +
-   "      One of three columns\n" +
-   "    </div>\n" +
-   "    <div class=\"col-4\">\n" +
-   "      One of three columns\n" +
-   "    </div>\n" +
-   "    <div class=\"col-4\">\n" +
-   "      One of three columns\n" +
-   "    </div>\n" +
-   "  </div>\n" +
+   "    <form>\n" +
+   "        <div class=\"row\">\n" +
+   "            <div class=\"col-2\">\n" +
+   "            </div>\n" +
+   "            <div class=\"col-8\">\n" +
+   "                <div class=\"form-group\">\n" +
+   "                    <label for=\"realname\">Name</label>\n" +
+   "                    <input type=\"text\" class=\"form-control\" id=\"realname\" placeholder=\"Write your name here\">\n" +
+   "                </div>\n" +
+   "            </div>\n" +
+   "            \n" +
+   "            <div class=\"col-2\">\n" +
+   "            </div>\n" +
+   "        </div>\n" +
+   "        <div class=\"row\">\n" +
+   "            <div class=\"col-2\">\n" +
+   "            </div>\n" +
+   "            <div class=\"col-8\">\n" +
+   "                {{#each questions}}\n" +
+   "                    <div class=\"form-group\">\n" +
+   "                        <label for=\"question{{this}}\">Question {{this}}</label>\n" +
+   "                        <textarea class=\"form-control\" id=\"question{{this}}\" placeholder=\"Write your first question here\"></textarea>\n" +
+   "                    </div>\n" +
+   "                {{/each}}\n" +
+   "            </div>\n" +
+   "            <div class=\"col-2\">\n" +
+   "            </div>\n" +
+   "        </div>\n" +
+   "        <div class=\"row\">\n" +
+   "            <button type=\"button\" class=\"btn btn-primary btn-sm\">I'm ready</button>\n" +
+   "        </div>\n" +
+   "    </form>\n" +
    "</div>\n" +
    "";
 
@@ -41,11 +62,8 @@ templates["lobby.handlebars"] = "<header>\n" +
    "                <hr class=\"primary\">\n" +
    "                <p>project made for Saturday/Sunday</p>\n" +
    "            </div>\n" +
-   "            <!-- <div class=\"col-lg-4 col-lg-offset-2 text-center\">\n" +
-   "                <i class=\"fa fa-phone fa-3x sr-contact\"></i>\n" +
-   "                <p></p>\n" +
-   "            </div> -->\n" +
-   "            <div class=\"col-lg-4 text-center\">\n" +
+   "        <div class=\"row\">\n" +
+   "            <div class=\"col-lg-8 col-lg-offset-2 text-center\">\n" +
    "                <i class=\"fa fa-github fa-4x\"></i>\n" +
    "                <p><a href=\"mailto:your-email@your-domain.com\">https://github.com/HackTheSouth2019</a></p>\n" +
    "            </div>\n" +
@@ -63,4 +81,93 @@ templates["see_asker.handlebars"] = "<h1>See Asker</h1>\n" +
 templates["spectating.handlebars"] = "<h1>Spectating</h1>\n" +
    "";
 
-templates["waiting_room.handlebars"] = "";
+templates["waiting_room.handlebars"] = "<div class=\"limiter\">\n" +
+   "		<div class=\"container-table100\">\n" +
+   "			<div class=\"wrap-table100\">\n" +
+   "					<div class=\"table\">\n" +
+   "\n" +
+   "						<div class=\"row header\">\n" +
+   "							<div class=\"cell\">\n" +
+   "								Full Name\n" +
+   "							</div>\n" +
+   "							<div class=\"cell\">\n" +
+   "								Questions\n" +
+   "							</div>\n" +
+   "						</div>\n" +
+   "\n" +
+   "						<div class=\"row\">\n" +
+   "							<div class=\"cell\" data-title=\"Full Name\">\n" +
+   "								Vincent Williamson\n" +
+   "							</div>\n" +
+   "							<div class=\"cell\" data-title=\"Questions\">\n" +
+   "								Ready\n" +
+   "							</div>\n" +
+   "						</div>\n" +
+   "\n" +
+   "						<div class=\"row\">\n" +
+   "							<div class=\"cell\" data-title=\"Full Name\">\n" +
+   "								Joseph Smith\n" +
+   "							</div>\n" +
+   "							<div class=\"cell\" data-title=\"Questions\">\n" +
+   "								Not Ready\n" +
+   "							</div>\n" +
+   "						</div>\n" +
+   "\n" +
+   "						<div class=\"row\">\n" +
+   "							<div class=\"cell\" data-title=\"Full Name\">\n" +
+   "								Justin Black\n" +
+   "							</div>\n" +
+   "							<div class=\"cell\" data-title=\"Questions\">\n" +
+   "								Ready\n" +
+   "							</div>\n" +
+   "						</div>\n" +
+   "\n" +
+   "						<div class=\"row\">\n" +
+   "							<div class=\"cell\" data-title=\"Full Name\">\n" +
+   "								Sean Guzman\n" +
+   "							</div>\n" +
+   "							<div class=\"cell\" data-title=\"Questions\">\n" +
+   "								Ready\n" +
+   "							</div>\n" +
+   "						</div>\n" +
+   "\n" +
+   "						<div class=\"row\">\n" +
+   "							<div class=\"cell\" data-title=\"Full Name\">\n" +
+   "								Keith Carter\n" +
+   "							</div>\n" +
+   "							<div class=\"cell\" data-title=\"Questions\">\n" +
+   "								Ready\n" +
+   "							</div>\n" +
+   "						</div>\n" +
+   "\n" +
+   "						<div class=\"row\">\n" +
+   "							<div class=\"cell\" data-title=\"Full Name\">\n" +
+   "								Austin Medina\n" +
+   "							</div>\n" +
+   "							<div class=\"cell\" data-title=\"Questions\">\n" +
+   "								Ready\n" +
+   "							</div>\n" +
+   "						</div>\n" +
+   "\n" +
+   "						<div class=\"row\">\n" +
+   "							<div class=\"cell\" data-title=\"Full Name\">\n" +
+   "								Vincent Williamson\n" +
+   "							</div>\n" +
+   "							<div class=\"cell\" data-title=\"Questions\">\n" +
+   "								Ready\n" +
+   "							</div>\n" +
+   "						</div>\n" +
+   "\n" +
+   "						<div class=\"row\">\n" +
+   "							<div class=\"cell\" data-title=\"Full Name\">\n" +
+   "								Joseph Smith\n" +
+   "							</div>\n" +
+   "							<div class=\"cell\" data-title=\"Location\">\n" +
+   "								Ready\n" +
+   "							</div>\n" +
+   "						</div>\n" +
+   "\n" +
+   "					</div>\n" +
+   "			</div>\n" +
+   "		</div>\n" +
+   "	</div>";
