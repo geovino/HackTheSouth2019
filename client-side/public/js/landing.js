19a05736f30853c4f35cb20a7d635aefe908d1d0
ivotimev <ivo.timev@gmail.com>
2019-02-10 05:07:52 +0000

    landinging

--- client-side/public/js/landing.js
+++ client-side/public/js/landing.js
@@ -1,6 +1,18 @@
+var xhttp = new XMLHttpRequest();
+
+
 $("#create-room").on("click", function(event) {
-  $("#link-present").removeClass("hidden");
-  $("#create-room").addClass("hidden");
-
-
+  $.ajax({
+      type: "post",
+      contentType: "application/json",
+      url: "http://3.8.68.131:8080/rooms/",
+      data: { 'number_of_players': 4 },
+      dataType: "json",
+      xhrFields: {
+        withCredentials: true
+      }
+    })
+    .done(function(data) {
+      console.log(data);
+    });
 });
