8a7a7d83203b98e55bdb13f4844ae4c639a65867
Nikolay Stanchev <ns@ip-172-26-8-156.eu-west-2.compute.internal>
2019-02-10 10:00:24 +0000

    Updates

--- server-side/bluetato_server/app.py
+++ server-side/bluetato_server/app.py
@@ -7,5 +7,5 @@ app_instance = Flask(__name__)
 app_instance.register_blueprint(rooms_blueprint)
 app_instance.config['CORS_HEADER'] = 'Content-Type'
 
-socketio = SocketIO(app_instance)
+socketio = SocketIO(app_instance, async_handlers=True)
 socketio.on_namespace(RoomsNamespace('/'))
